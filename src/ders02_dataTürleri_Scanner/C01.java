31bc7c43b919e40c13a4d47e783ea0696cdba16b
Demet Gundogdu <demetgundogdu4@gmail.com>
2023-05-08 14:59:51 +0300

    2. gün sonu

---
+++ src/ders02_dataTürleri_Scanner/C01.java
@@ -0,0 +1,31 @@
+package ders02_dataTürleri_Scanner;
+
+public class C01 {
+    public static void main(String[] args) {
+         /*
+        Java RunTime programdir
+        Yani biz yesil OK'a basip calisytirdigimizda
+        hersey sifirdan baslar
+
+        class'in sonuna kadar tum kodlar
+        yukaridan asagi calistirilir
+
+        class bittiginde hersey SIFIRLANIR
+     */
+
+        int bakiye=0;
+
+
+
+        bakiye=200;
+
+        System.out.println( bakiye ); // 200
+
+        bakiye=500;
+
+
+        bakiye=300;
+
+        System.out.println( bakiye ); // 300
+    }
+}
---
+++ src/ders02_dataTürleri_Scanner/C04.java
@@ -0,0 +1,22 @@
+package ders02_dataTürleri_Scanner;
+
+import java.util.Scanner;
+
+public class C04 {
+    public static void main(String[] args) {
+        // 1.adim : Scanner objesi olusturun
+        Scanner scan = new Scanner(System.in);
+
+        // 2.adim : Kullaniciya ne istediginizi soyleyin
+        System.out.println("Lutfen isminizi giriniz");
+
+        // 3. adim : girilen bilgiyi icine koyabilecegimiz bir variable olusturun
+        //           olusturdugumuz scanner objesi ile uygun method'u kullanarak bilgiyi alin
+
+        String kullaniciIsmi = scan.next();
+
+
+        System.out.println("Girilen isim : " + kullaniciIsmi);
+
+    }
+}
