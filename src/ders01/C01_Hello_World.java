56674001658cde0ad74f8228105a8144c015b8f6
Demet Gundogdu <demetgundogdu4@gmail.com>
2023-05-08 14:47:51 +0300

    1. gün sonu

---
+++ src/ders01/C01_Hello_World.java
@@ -0,0 +1,9 @@
+package ders01;
+
+public class C01_Hello_World {
+    public static void main(String[] args) {
+
+
+        System.out.println("hello world");
+    }
+}
---
+++ src/ders01/C02_VARIABLE.java
@@ -0,0 +1,25 @@
+package ders01;
+
+public class C02_VARIABLE {
+    public static void main(String[] args) {
+        int bakiye =0;
+
+        bakiye =200;
+        System.out.println(bakiye);  //200
+
+        bakiye =500;
+
+        bakiye=300;
+
+        System.out.println(bakiye);  //300
+
+
+
+        /*
+        java RunTime programdır
+        yani biz yeşil ok basıp
+        HER SEY SIFIRDAN BASLAR
+
+         */
+    }
+}
