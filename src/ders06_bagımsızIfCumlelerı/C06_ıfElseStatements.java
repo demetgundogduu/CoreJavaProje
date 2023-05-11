f6dce074cbeb059cd044819bc6351397a4307ac9
Demet Gundogdu <demetgundogdu4@gmail.com>
2023-05-11 22:12:24 +0300

    4.gün sonu

---
+++ src/ders06_bagımsızIfCumlelerı/C06_ıfElseStatements.java
@@ -0,0 +1,47 @@
+package ders06_bagımsızIfCumlelerı;
+
+import java.util.Scanner;
+
+public class C06_ıfElseStatements {
+    public static void main(String[] args) {
+
+
+
+        // Soru 3- Kullanicidan yasini isteyin,
+        //         65 yas ve uzeri ise ”Emekli olabilirsin” yazdirin,
+        //         yoksa emekli olmasi icin calismasi gereken yil sayisini yazdirin.
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lutfen yasinizi giriniz");
+
+        double yas= scan.nextDouble();
+
+        if (yas >= 65){
+            System.out.println("Emekli olabilirsin");
+        } else {
+
+            System.out.println("Emekli olabilmek icin daha " +(65-yas)+ " yil calismalisin");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/ders06_bagımsızIfCumlelerı/C07_ıfElseStatements.java
@@ -0,0 +1,49 @@
+package ders06_bagımsızIfCumlelerı;
+
+import java.util.Scanner;
+
+public class C07_ıfElseStatements {
+
+    public static void main(String[] args) {
+        //Soru 4- Kullanicidan bir karakter girmesini isteyin,
+        //        girilen karakterin buyuk harf olup olmadigini yazdirin.
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lutfen bir karakter giriniz");
+
+        char krk = scan.next().charAt(0);
+
+        // r ===> A ile Z arasinda mi ?
+
+        if ('A'<= krk && krk <= 'Z'){
+            System.out.println("Girilen karakter buyuk harf");
+        }else{
+            System.out.println("Girilen karakter buyuk harf degil");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
