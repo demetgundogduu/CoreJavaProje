0292c822991f8609dd036df4d49d3370071b10ff
Demet Gundogdu <demetgundogdu4@gmail.com>
2023-05-19 23:05:44 +0300

    7 gün sonu

---
+++ src/ders10_stringManıpulation/C02_contains.java
@@ -0,0 +1,21 @@
+package ders10_stringManıpulation;
+
+public class C02_contains {
+    public static void main(String[] args) {
+        String str= "Java ogren, isi kap";
+
+        System.out.println(str.contains("Java")); // true
+
+        String s="ka";
+
+        System.out.println(str.contains(s)); // true
+
+        System.out.println(str.contains("a")); // true
+
+        System.out.println(str.contains(" ")); // true
+
+        System.out.println(str.contains("")); // true
+
+        System.out.println(str.contains("java")); // false
+    }
+}
