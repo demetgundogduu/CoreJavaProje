e36bdadf72f3029326f1c2bde95f945d994a69bd
Demet Gundogdu <demetgundogdu4@gmail.com>
2023-06-19 22:28:27 +0300

    13.gün sonu

---
+++ src/ders13_MethodOlusturma/C01_methodOlusturma.java
@@ -0,0 +1,13 @@
+package ders13_MethodOlusturma;
+
+public class C01_methodOlusturma {
+    public static void main(String[] args) {
+        String str= "Java Candir";
+
+        // str'i buyuk harflere cevirin
+
+        str = str.toUpperCase();
+
+        System.out.println(str.toLowerCase()); // java candir
+    }
+}
---
+++ src/ders13_MethodOlusturma/C02_ikiSayıToplamaMethodu.java
@@ -0,0 +1,29 @@
+package ders13_MethodOlusturma;
+
+import java.util.Scanner;
+
+public class C02_ikiSayıToplamaMethodu {
+    public static void main(String[] args) {
+
+        // kullanicidan 2 sayi alip
+        // toplamlarini yazdiran bir method olusturun
+
+        ikiSayiTopla();
+        ikiSayiTopla();
+    }
+
+
+
+
+
+
+
+    public static void ikiSayiTopla(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Toplamak uzere 2 sayi giriniz");
+        double sayi1= scan.nextDouble();
+        double sayi2= scan.nextDouble();
+
+        System.out.println("Iki sayinin toplami : " + (sayi1+sayi2));
+    }
+}
