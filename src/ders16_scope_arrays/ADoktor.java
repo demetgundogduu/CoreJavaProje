c0096ae5eca9520bd9c647a33d927957d3d1e630
Demet Gundogdu <demetgundogdu4@gmail.com>
2023-06-22 23:56:27 +0300

    16 .gun sonu

---
+++ src/ders16_scope_arrays/ADoktor.java
@@ -0,0 +1,11 @@
+package ders16_scope_arrays;
+
+public class ADoktor {
+    String isim;
+    String soyisim;
+    int dogumYili;
+    boolean askerlikYaptiMi;
+    String telNo;
+
+    static String fakulte="Tip";
+}
